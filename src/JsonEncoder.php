84cb4e28f8bc81eb51ed7e69ab6c7b702cda6f7f
Andy Fletcher <andy@architechlabs.com>
2022-08-30 09:04:10 +0100

    JsonEncoder
    All our functionality is now in the Trait.
    
    The only complicating factor here is that we make the otherwise private
    methods pulic.

--- src/JsonEncoder.php
+++ src/JsonEncoder.php
@@ -4,33 +4,10 @@ declare(strict_types=1);
 
 namespace Leverage\Encoder;
 
-use JsonException;
-use JsonSerializable;
-
 class JsonEncoder
 {
-    /**
-     * @throws JsonException
-     */
-    public function decode(
-        string $json,
-    ): mixed {
-        return json_decode(
-            json: $json,
-            associative: true,
-            flags: JSON_THROW_ON_ERROR,
-        );
-    }
-
-    /**
-     * @throws JsonException
-     */
-    public function encode(
-        array | JsonSerializable $data,
-    ): string {
-        return json_encode(
-            value: $data,
-            flags: JSON_THROW_ON_ERROR
-        );
+    use JsonTrait {
+        decode as public;
+        encode as public;
     }
 }
